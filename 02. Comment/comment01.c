6bbf46fd1b4f64b0e5d5b06a41677b840aaa6899
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-01-02 16:15:45 +0900

    [Init] project start

--- 02. Comment/comment01.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-	// printf("Hello, World!\n");
-	printf("Hi, Human \n");
-	printf("Hi, Computer \n");
-
-	return 0;
-}
--- 02. Comment/variable01.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-	int a;
-	a = 10;
-	printf("a의 값은? : %d \n", a);
-	return 0;
-}
