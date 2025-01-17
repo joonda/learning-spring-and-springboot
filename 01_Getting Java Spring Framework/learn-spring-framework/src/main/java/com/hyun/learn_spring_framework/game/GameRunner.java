7346a27bafe6d4ef9577504d7759aacff83a579e
joonda <kloudzero@kakao.com>
2025-01-17 23:52:18 +0900

    [Fix] fix Annotation

--- 01_Getting Java Spring Framework/learn-spring-framework/src/main/java/com/hyun/learn_spring_framework/game/GameRunner.java
+++ 01_Getting Java Spring Framework/learn-spring-framework/src/main/java/com/hyun/learn_spring_framework/game/GameRunner.java
@@ -1,7 +1,8 @@
 package com.hyun.learn_spring_framework.game;
 
 public class GameRunner {
-    private SuperContraGame game; // 의존성
+    private SuperContraGame game; // 외부에서 접근 불가 (변수 관리 차원)
+
     public GameRunner(SuperContraGame game) { // 생성자를 통한 의존성 주입
         this.game = game;
     }
