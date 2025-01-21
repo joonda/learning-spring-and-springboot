8a124499e9eaebf89e045917cc0d43f20b79827f
HJ Lee <143615224+joonda@users.noreply.github.com>
2025-01-21 18:43:12 +0900

    [Update] section 3 upload

---
+++ 00_module/learn-spring-framework-02/src/main/java/com/hyun/learn_spring_framework/examples/f1/SimpleSpringContextLauncherApplication.java
@@ -0,0 +1,20 @@
+package com.hyun.learn_spring_framework.examples.a0;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan // package 내에서 ComponentScan을 자동적으로 수행한다, 경로설정이 필요없다!
+public class SimpleSpringContextLauncherApplication {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class);
+
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+    }
+}
